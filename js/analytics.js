d45a2c1415ea32e911c28193e2f5934ad0ed34d2
livresoumaiseu <livresoumaiseu@gmail.com>
2025-08-18 15:29:38 -0300

    updating

---
+++ js/analytics.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+(function(){
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = "https://www.googletagmanager.com/gtag/js?id=G-89LTSFZN9G";
+  document.head.appendChild(s);
+  gtag('js', new Date());
+  gtag('config', 'G-89LTSFZN9G');
+})();
